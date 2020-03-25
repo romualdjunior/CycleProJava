8c66b3d8bdb2a7dd6f4b42a43be3d3912cd9bc1b
romualdjunior <romualdmotcheho@yahoo.com>
2020-03-25 21:33:36 +0100

    je viens de créer ce projet

---
+++ CyclePro/src/GUI/Login/Login.java
@@ -0,0 +1,23 @@
+package GUI.Login;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Login extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        primaryStage.setTitle("CyclePro");
+        primaryStage.setScene(new Scene(root, 700, 700));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
